ba0d668a87340b45df1ea8223b729a911a3ce875
nebowden <nathanebowden@gmail.com>
2015-01-06 09:35:15 -0600

    cleaned up and organized js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-
 	
 
 	var url = "https://www.googleapis.com/youtube/v3/search";
 
+	/* search youtube API using user input from form */
 	var getRequest = function(userSearch) {
 
 		var params = {
@@ -15,27 +15,32 @@ $(document).ready(function(){
 
 		$.getJSON(url, params, function(data){
 	    	showResults(data.items);
-	    	//console.log(data);
 		});
 
 	};
 
+	/* append thumbnail and link to results div*/
+
 	var showResults = function(results) {
 
 		$.each(results, function(index, value){
-				//console.log(value.snippet.thumbnails.default.url);
-				//console.log(value.id.videoId);
 				$("#results-container").append("<a href='https://www.youtube.com/watch?v=" + value.id.videoId + "' target='_blank'><img src='" + value.snippet.thumbnails.default.url + "'></a>");
 			});
 		};
+
+	/* clear results div*/
 
 	var clearResults = function(){
 		$("#results-container").find('a', 'img').remove();
 	};
 
+	/* clear searcj field on double-slick*/
+
 	$("#query").dblclick(function(){
 		$(this).val('');
 	});
+
+	/* function on user's submission of search query */
 
 	$("#search-form").submit(function(event) {
 		var userSearch = $("#query").val();
